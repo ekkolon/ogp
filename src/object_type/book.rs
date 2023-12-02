d9a423101476b87a3ac62197ecc382c9b7e246f5
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2023-12-02 19:50:27 +0100

    feat: add `Book` object type struct

--- src/object_type/book.rs
+++ src/object_type/book.rs
@@ -1,3 +1,41 @@
 //! Metadata utility for the Open Graph `book` meta tag.
 
-// TODO: implementation
+use crate::{
+  builder::{Metadata, MetadataBuilder, ObjectMetadata},
+  error::Error,
+  metadata::{audio, image, video},
+  object_type::ObjectType,
+  Result,
+};
+use serde::{de::IntoDeserializer, Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default, Debug, Clone)]
+pub struct Book {
+  /// Who wrote this book.
+  #[serde(rename = "book:author")]
+  pub author: Option<String>,
+
+  /// The ISBN.
+  #[serde(rename = "book:isbn")]
+  pub isbn: Option<String>,
+
+  /// The date the book was released.
+  #[serde(rename = "book:release_date ")]
+  pub release_date: Option<String>,
+
+  /// Tag words associated with this book.
+  #[serde(rename = "book:tag")]
+  pub tag: Option<String>,
+
+  #[serde(flatten)]
+  metadata: ObjectMetadata,
+}
+
+impl Book {
+  pub fn new() -> Self {
+    Book {
+      metadata: MetadataBuilder::with_type(ObjectType::Book),
+      ..Default::default()
+    }
+  }
+}
