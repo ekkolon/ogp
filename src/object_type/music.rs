77d41180a44ebfffe5fdd8963d654258e6c56f3e
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2023-12-02 19:50:34 +0100

    feat: add `Music` object type struct

--- src/object_type/music.rs
+++ src/object_type/music.rs
@@ -1,3 +1,70 @@
 //! Metadata utility for the Open Graph `music` meta tag.
 
-// TODO: implementation
+use crate::{
+  builder::{Metadata, MetadataBuilder, ObjectMetadata},
+  error::Error,
+  metadata::{audio, image, video},
+  object_type::ObjectType,
+  Result,
+};
+use serde::{de::IntoDeserializer, Deserialize, Serialize};
+
+// TODO: Add missing props
+#[derive(Serialize, Deserialize, Default, Debug, Clone)]
+pub struct MusicAlbum {
+  #[serde(flatten)]
+  metadata: ObjectMetadata,
+}
+
+impl MusicAlbum {
+  pub fn new() -> Self {
+    MusicAlbum {
+      metadata: MetadataBuilder::with_type(ObjectType::MusicAlbum),
+    }
+  }
+}
+
+// TODO: Add missing props
+#[derive(Serialize, Deserialize, Default, Debug, Clone)]
+pub struct MusicSong {
+  #[serde(flatten)]
+  metadata: ObjectMetadata,
+}
+
+impl MusicSong {
+  pub fn new() -> Self {
+    MusicSong {
+      metadata: MetadataBuilder::with_type(ObjectType::MusicSong),
+    }
+  }
+}
+
+// TODO: Add missing props
+#[derive(Serialize, Deserialize, Default, Debug, Clone)]
+pub struct MusicPlaylist {
+  #[serde(flatten)]
+  metadata: ObjectMetadata,
+}
+
+impl MusicPlaylist {
+  pub fn new() -> Self {
+    MusicPlaylist {
+      metadata: MetadataBuilder::with_type(ObjectType::MusicPlaylist),
+    }
+  }
+}
+
+// TODO: Add missing props
+#[derive(Serialize, Deserialize, Default, Debug, Clone)]
+pub struct MusicRadioStation {
+  #[serde(flatten)]
+  metadata: ObjectMetadata,
+}
+
+impl MusicRadioStation {
+  pub fn new() -> Self {
+    MusicRadioStation {
+      metadata: MetadataBuilder::with_type(ObjectType::MusicRadioStation),
+    }
+  }
+}
