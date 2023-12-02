56ada0cd41f7148826a6c9da27f8f2fa3ee42775
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2023-12-02 19:50:17 +0100

    feat: add `Article` object type struct

--- src/object_type/article.rs
+++ src/object_type/article.rs
@@ -1,3 +1,49 @@
 //! Metadata utility for the Open Graph `article` meta tag.
 
-// TODO: implementation
+use crate::{
+  builder::{Metadata, MetadataBuilder, ObjectMetadata},
+  error::Error,
+  metadata::{audio, image, video},
+  object_type::ObjectType,
+  Result,
+};
+use serde::{de::IntoDeserializer, Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default, Debug, Clone)]
+pub struct Article {
+  /// When the article was first published.
+  #[serde(rename = "article:published_time")]
+  pub published_time: Option<String>,
+
+  /// When the article was last changed.
+  #[serde(rename = "article:modified_time")]
+  pub modified_time: Option<String>,
+
+  /// When the article is out of date after.
+  #[serde(rename = "article:expiration_time")]
+  pub expiration_time: Option<String>,
+
+  /// Writers of the article.
+  #[serde(rename = "article:author")]
+  pub author: Option<String>,
+
+  /// A high-level section name. E.g. Technology
+  #[serde(rename = "article:section")]
+  pub section: Option<String>,
+
+  /// Tag words associated with this article.
+  #[serde(rename = "article:tag")]
+  pub tag: Option<String>,
+
+  #[serde(flatten)]
+  metadata: ObjectMetadata,
+}
+
+impl Article {
+  pub fn new() -> Self {
+    Article {
+      metadata: MetadataBuilder::with_type(ObjectType::Article),
+      ..Default::default()
+    }
+  }
+}
