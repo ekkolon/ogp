87a606099a9693aae125455a10aa898300d54742
Nelson Dominguez <ekkolon@proton.me>
2023-12-05 02:22:03 +0100

    feat: add `WebsiteMetadataBuilder`

--- src/object_type/website.rs
+++ src/object_type/website.rs
@@ -1,14 +1,31 @@
 // TODO: Add docs
 
-use crate::{builder::MetadataBuilder, object_type::ObjectType, Result};
+use crate::{
+  builder::MetadataBuilder, convert::ToHTML, object_type::ObjectType, Result,
+};
 use serde::{de::IntoDeserializer, Deserialize, Serialize};
+use serde_json::json;
+
+#[derive(Serialize, Deserialize, Default, Debug, Clone)]
+pub struct WebsiteMetadataBuilder {
+  #[serde(flatten)]
+  metadata: MetadataBuilder,
+}
 
 pub trait Website {
-  fn website(&self) -> MetadataBuilder;
+  fn website(&self) -> WebsiteMetadataBuilder;
 }
 
 impl Website for MetadataBuilder {
-  fn website(&self) -> Self {
-    self.clone()
+  fn website(&self) -> WebsiteMetadataBuilder {
+    WebsiteMetadataBuilder {
+      metadata: MetadataBuilder {
+        object_type: ObjectType::Website,
+        metadata: self.get().clone(),
+      },
+      ..Default::default()
+    }
   }
 }
+
+impl ToHTML for WebsiteMetadataBuilder {}
