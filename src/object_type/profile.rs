53d0982ebcf6e9f5e32bbb3df3a7342a011cfeeb
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2024-01-03 10:54:51 +0100

    feat(profile): add `Profile` metadata builder

--- src/object_type/profile.rs
+++ src/object_type/profile.rs
@@ -1,6 +1,8 @@
 //! Metadata utility for the Open Graph `profile` meta tag.
 
-use crate::{builder::MetadataBuilder, object_type::ObjectType, Result};
+use crate::{
+  builder::MetadataBuilder, convert::ToHTML, object_type::ObjectType, Result,
+};
 use serde::{de::IntoDeserializer, Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -13,7 +15,7 @@ pub enum Gender {
 }
 
 #[derive(Serialize, Deserialize, Default, Debug, Clone)]
-pub struct Profile {
+pub struct ProfileMetadataBuilder {
   /// A name normally given to an individual by a parent or self-chosen.
   #[serde(rename = "profile:first_name")]
   pub first_name: Option<String>,
@@ -35,11 +37,22 @@ pub struct Profile {
   metadata: MetadataBuilder,
 }
 
-impl Profile {
-  pub fn new() -> Self {
-    Profile {
-      metadata: MetadataBuilder::with_type(ObjectType::Profile),
+impl ProfileMetadataBuilder {}
+
+pub trait Profile {
+  fn profile(&self) -> ProfileMetadataBuilder;
+}
+
+impl Profile for MetadataBuilder {
+  fn profile(&self) -> ProfileMetadataBuilder {
+    ProfileMetadataBuilder {
+      metadata: MetadataBuilder {
+        object_type: ObjectType::Profile,
+        metadata: self.get().clone(),
+      },
       ..Default::default()
     }
   }
 }
+
+impl ToHTML for ProfileMetadataBuilder {}
